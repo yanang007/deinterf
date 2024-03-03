fc1e553faaaa360b2abed185d3d517cb750bb569
dorian-li <49279922+dorian-li@users.noreply.github.com>
2024-03-04 01:14:54 +0800

    :memo: IOC数据形式下的新tmi补偿示例

--- examples/compensate_tmi.py
+++ examples/compensate_tmi.py
@@ -1,23 +1,31 @@
-from pathlib import Path
-
 import matplotlib.pyplot as plt
-import pandas as pd
+from sgl2020 import Sgl2020
 
 from deinterf.compensator.tmi.linear import Terms, TollesLawson
-from deinterf.data import DataGroup, MagVector
+from deinterf.foundation import DataIOC
+from deinterf.foundation.sensors import MagVector, Tmi
 from deinterf.metrics.fom import improve_rate, noise_level
 
 if __name__ == "__main__":
-    flt_d = pd.read_csv(Path(__file__).resolve().parent / "test_data.csv")
+    surv_d = (
+        Sgl2020()
+        .line(["1002.02"])
+        .source(
+            [
+                "flux_b_x",
+                "flux_b_y",
+                "flux_b_z",
+                "mag_3_uc",
+            ]
+        )
+        .take()
+    )
+    flt_d = surv_d["1002.02"]
 
     # 数据准备
-    tmi_with_interf = flt_d["mag_3_uc"]
-    mag_vec = MagVector().fit(flt_d, bx="flux_b_x", by="flux_b_y", bz="flux_b_z")
-    fom_data = DataGroup(
-        (
-            mag_vec.to_tmi(),
-            mag_vec.to_dir_cosine(),
-        )
+    tmi_with_interf = Tmi(tmi=flt_d["mag_3_uc"])
+    fom_data = DataIOC().add(
+        MagVector(bx=flt_d["flux_b_x"], by=flt_d["flux_b_y"], bz=flt_d["flux_b_z"])
     )
 
     # 创建补偿器
@@ -28,20 +36,20 @@ if __name__ == "__main__":
     tmi_clean = compensator.transform(fom_data, tmi_with_interf)
 
     # 或者一步到位，拟合与补偿自身
-    # tmi_pure = compensation.fit_transform(fom_data, tmi_with_interf)
+    tmi_clean = compensator.fit_transform(fom_data, tmi_with_interf)
 
     # 仅预测磁干扰
     interf = compensator.predict(fom_data)
 
     # 评估磁补偿性能
-    comped_noise_level = noise_level(tmi_clean)
+    comped_noise_level = noise_level(tmi_clean.data)
     print(f"{comped_noise_level=}")
 
-    ir = improve_rate(tmi_with_interf, tmi_clean)
+    ir = improve_rate(tmi_with_interf.data, tmi_clean.data)
     print(f"{ir=}")
 
     # 简要绘图
-    plt.plot(tmi_with_interf, label="tmi_with_interf")
-    plt.plot(tmi_clean, label="tmi_clean")
+    plt.plot(tmi_with_interf.data, label="tmi_with_interf")
+    plt.plot(tmi_clean.data, label="tmi_clean")
     plt.legend()
     plt.show()

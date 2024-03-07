01991c15def5bef433fc06a90a94878d07fa9c1e
dorian-li <49279922+dorian-li@users.noreply.github.com>
2024-03-07 21:02:00 +0800

    :memo: 以拓展INS姿态为例展示用法

--- examples/compensate_tmi_extended.py
+++ examples/compensate_tmi_extended.py
@@ -1,12 +1,78 @@
+from datetime import datetime, timedelta
+
 import matplotlib.pyplot as plt
+import numpy as np
+import ppigrf
 from numpy.typing import ArrayLike
+from scipy.spatial.transform import Rotation as R
 from sgl2020 import Sgl2020
 
 from deinterf.compensator.tmi.linear import Terms, TollesLawson
-from deinterf.foundation.sensors import MagVector, Tmi, DirectionalCosine
-from deinterf.metrics.fom import improve_rate, noise_level
+from deinterf.foundation.sensors import DirectionalCosine, MagVector, Tmi
+from deinterf.metrics.fom import improve_rate
 from deinterf.utils.data_ioc import DataIoC, DataNDArray
 from deinterf.utils.transform import magvec2dircosine
+
+
+class WGS84(DataNDArray):
+    def __new__(cls, lon: ArrayLike, lat: ArrayLike, alt: ArrayLike):
+        return super().__new__(cls, lon, lat, alt)
+
+    @property
+    def lon(self):
+        # unit: degree
+        return self[:, 0]
+
+    @property
+    def lat(self):
+        # unit: degree
+        return self[:, 1]
+
+    @property
+    def alt(self):
+        # unit: meter
+        return self[:, 2]
+
+
+class Date(DataNDArray):
+    def __new__(cls, doy: ArrayLike, year: ArrayLike):
+        return super().__new__(cls, doy, year)
+
+    @property
+    def doy(self):
+        return self[:, 0]
+
+    @property
+    def year(self):
+        return self[:, 1]
+
+
+class IGRF(DataNDArray):
+    @classmethod
+    def __build__(cls, container: DataIoC):
+        lon = container[WGS84].lon
+        lat = container[WGS84].lat
+        alt = container[WGS84].alt / 1000.0  # unit: km
+        # doy to datetime
+        year = container[Date].year
+        doy = container[Date].doy
+        date = datetime(int(year[0]), 1, 1) + timedelta(days=int(doy[0]) - 1)
+
+        geo_e, geo_n, geo_u = ppigrf.igrf(lon, lat, alt, date)
+        geo = np.row_stack((geo_e, geo_n, geo_u)).T
+        return cls(*geo.T)
+
+    @property
+    def east(self):
+        return self[:, 0]
+
+    @property
+    def north(self):
+        return self[:, 1]
+
+    @property
+    def up(self):
+        return self[:, 2]
 
 
 class InertialAttitude(DataNDArray):
@@ -28,9 +94,16 @@ class InertialAttitude(DataNDArray):
 
 class InsDirectionalCosine(DirectionalCosine):
     @classmethod
-    def __build__(cls, container: DataIOC) -> DirectionalCosine:
-        dir_cosine = magvec2dircosine(container[InertialAttitude])
-        return cls(*dir_cosine.T)
+    def __build__(cls, container: DataIoC) -> DirectionalCosine:
+        att_angle = container[InertialAttitude]  # (yaw, pitch, roll): DEN
+        # DEN to ENU
+        att_angle = att_angle[:, [1, 2, 0]]
+        att_angle[:, 2] = -att_angle[:, 2]
+
+        r = R.from_euler("xyz", att_angle, degrees=True)
+        geo_bodyframe = r.apply(container[IGRF], inverse=True)
+        dcos = magvec2dircosine(geo_bodyframe)
+        return cls(*dcos.T)
 
 
 if __name__ == "__main__":
@@ -39,49 +112,54 @@ if __name__ == "__main__":
         .line(["1002.02"])
         .source(
             [
-                "flux_b_x",
-                "flux_b_y",
-                "flux_b_z",
+                "flux_d_x",
+                "flux_d_y",
+                "flux_d_z",
                 "mag_3_uc",
-                'ins_yaw',
-                'ins_pitch',
-                'ins_roll',
+                "ins_yaw",
+                "ins_pitch",
+                "ins_roll",
+                "lon",
+                "lat",
+                "utm_z",
             ]
         )
         .take()
     )
     flt_d = surv_d["1002.02"]
+    # date of flt1002
+    flt_d["doy"] = 172
+    flt_d["year"] = 2020
 
-    # 数据准备
+    # classic compensation
     tmi_with_interf = Tmi(tmi=flt_d["mag_3_uc"])
     fom_data = DataIoC().with_data(
-        MagVector[1](bx=flt_d["flux_b_x"], by=flt_d["flux_b_y"], bz=flt_d["flux_b_z"]),
-        InertialAttitude[1](yaw=flt_d["ins_yaw"], pitch=flt_d["ins_pitch"], roll=flt_d["ins_roll"]),
+        MagVector[1](bx=flt_d["flux_d_x"], by=flt_d["flux_d_y"], bz=flt_d["flux_d_z"]),
+    )
+
+    compensator = TollesLawson(terms=Terms.Terms_16[1])
+    tmi_clean_classic = compensator.fit_transform(fom_data, tmi_with_interf)
+
+    # INS extended compensation
+    fom_data = DataIoC().with_data(
+        Date[1](doy=flt_d["doy"], year=flt_d["year"]),
+        WGS84[1](lon=flt_d["lon"], lat=flt_d["lat"], alt=flt_d["utm_z"]),
+        InertialAttitude[1](
+            yaw=flt_d["ins_yaw"], pitch=flt_d["ins_pitch"], roll=flt_d["ins_roll"]
+        ),
+        MagVector[1](bx=flt_d["flux_d_x"], by=flt_d["flux_d_y"], bz=flt_d["flux_d_z"]),
     )
     fom_data.add_provider(DirectionalCosine, InsDirectionalCosine)
+    tmi_clean_ins = compensator.fit_transform(fom_data, tmi_with_interf)
 
-    # 创建补偿器
-    compensator = TollesLawson(terms=Terms.Terms_16[1])
-    compensator.fit(fom_data, tmi_with_interf)
+    # compare
+    ir_classic = improve_rate(tmi_with_interf, tmi_clean_classic, verbose=True)
+    print(f"{ir_classic=}")
+    ir_ins = improve_rate(tmi_with_interf, tmi_clean_ins, verbose=True)
+    print(f"{ir_ins=}")
 
-    # 补偿给定信号
-    tmi_clean = compensator.transform(fom_data, tmi_with_interf)
-
-    # 或者一步到位，拟合与补偿自身
-    tmi_clean = compensator.fit_transform(fom_data, tmi_with_interf)
-
-    # 仅预测磁干扰
-    interf = compensator.predict(fom_data)
-
-    # 评估磁补偿性能
-    comped_noise_level = noise_level(tmi_clean)
-    print(f"{comped_noise_level=}")
-
-    ir = improve_rate(tmi_with_interf, tmi_clean)
-    print(f"{ir=}")
-
-    # 简要绘图
     plt.plot(tmi_with_interf, label="tmi_with_interf")
-    plt.plot(tmi_clean, label="tmi_clean")
+    plt.plot(tmi_clean_classic, label="tmi_clean_classic")
+    plt.plot(tmi_clean_ins, label="tmi_clean_ins")
     plt.legend()
     plt.show()
